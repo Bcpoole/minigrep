a9b9bdbefb1507ae6ebf8592d6a24982a684cf3b
Brandon Poole <brandoncpoole@gmail.com>
2018-10-20 18:57:02 -0700

    Added case insensitive searching option.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Chapter 12 project for https://doc.rust-lang.org/book/2018-edition/
 
 Basic grep functionality. First command line arg is the pattern and second is the filename.
+
+Having an ENV var set and not equal to "0" or "false" will do case-insensitive searching.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 use std::io::prelude::*;
@@ -5,6 +6,7 @@ use std::io::prelude::*;
 pub struct Config {
     pub query: String,
     pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -16,7 +18,23 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE");
+        let case_sensitive = if case_sensitive.is_err() {
+            false
+        } else {
+            let val = case_sensitive.unwrap();
+            if val == "0" || val == "false" {
+                true
+            } else {
+                false
+            }
+        };
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -32,10 +50,29 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents) {
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
 
@@ -56,28 +93,34 @@ mod tests {
         let config = Config::new(args).unwrap();
 
         assert_eq!(config.query, "s");
-        assert_eq!(config.filename, "file.txt")
+        assert_eq!(config.filename, "file.txt");
     }
 
     #[test]
-    fn runs() {
-        let args = &vec![
-            String::from("lib.rs"),
-            String::from("s"),
-            String::from("file.txt"),
-        ];
-        let config = Config::new(args).unwrap();
-    }
-
-    #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUst";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 
     #[test]
@@ -85,6 +128,6 @@ Pick three.";
         let query = "a";
         let contents = "";
 
-        assert!(search(query, contents).is_empty())
+        assert!(search(query, contents).is_empty());
     }
 }
