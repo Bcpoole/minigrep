7b1e7f8356a0e1c38eacf193e981084534a9c171
Brandon Poole <brandoncpoole@gmail.com>
2018-10-20 18:24:05 -0700

    Added unit tests and search functionality. Removed debug print statements.

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,71 @@ impl Config {
     }
 }
 
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
 
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_config() {
+        let args = &vec![
+            String::from("lib.rs"),
+            String::from("s"),
+            String::from("file.txt"),
+        ];
+        let config = Config::new(args).unwrap();
+
+        assert_eq!(config.query, "s");
+        assert_eq!(config.filename, "file.txt")
+    }
+
+    #[test]
+    fn runs() {
+        let args = &vec![
+            String::from("lib.rs"),
+            String::from("s"),
+            String::from("file.txt"),
+        ];
+        let config = Config::new(args).unwrap();
+    }
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn no_results() {
+        let query = "a";
+        let contents = "";
+
+        assert!(search(query, contents).is_empty())
+    }
 }
